3ff47942e93595fa702e99aa443daf3cb34b3162
David Chu <david.f.chu@hotmail.com>
2020-11-18 22:08:33 -0500

    updated Downloader.py to be able to run seperately. Also saves as csv instead of pkl file. Removed filter by default so will save entire response

--- Downloader.py
+++ Downloader.py
@@ -2,9 +2,11 @@
 Using the [psaw library](https://github.com/dmarx/psaw) to fetch comments from [pushshift.io api](https://github.com/pushshift/api)
 """
 #!pip install psaw
-
+import pdb
 import pandas as pd
 import datetime as dt
+import os
+import sys
 from tqdm import tqdm
 from psaw import PushshiftAPI
 
@@ -14,10 +16,13 @@ class Downloader:
   filter_fields=["body","score","subreddit"]
   api = None
   
-  def __init__(self, save_local = True, max_comment_count = 100000):
+  def __init__(self, save_local = True, outpath = None, max_comment_count = 100000, compress=False, filter_fields = []):
     self.save_local = save_local
     self.max_comment_count = max_comment_count
-    self.api = PushshiftAPI()
+    self.api = PushshiftAPI(rate_limit_per_minute = 60, max_results_per_request = 100)
+    self.outpath = outpath
+    self.compress = compress
+    self.filter_fields = filter_fields
 
   """
   grabs the subreddit and returns the comments as a dataframe
@@ -29,12 +34,20 @@ class Downloader:
     comments = []
     
     # rate limit = 1 per second, default
-    # comment limit = 500 per requestion, default
-    gen = self.api.search_comments(
-        subreddit=subreddit_name,
-        filter=self.filter_fields,
-        before=before_epoch
-    )
+    # comment limit = 100 per request, default
+
+    # check for filter
+    if len(self.filter_fields) > 0:
+      gen = self.api.search_comments(
+          subreddit=subreddit_name,
+          filter=self.filter_fields,
+          before=before_epoch
+      )
+    else:
+      gen = self.api.search_comments(
+          subreddit=subreddit_name,
+          before=before_epoch
+      )
 
     with tqdm(total=self.max_comment_count) as pbar:
       for c in gen:
@@ -43,15 +56,28 @@ class Downloader:
         if len(comments) >= self.max_comment_count:
           break
 
-    df_subreddit = pd.DataFrame([thing.d_ for thing in comments]).drop(["created_utc", "created"], axis=1)
+    df_subreddit = pd.DataFrame([thing.d_ for thing in comments])#.drop(["created_utc", "created"], axis=1)
     df_subreddit = df_subreddit[~(df_subreddit.body.isin(["[removed]","[deleted]"])) & (df_subreddit.score > 0)]
 
     """Pickle the data for download if necessary"""
     if self.save_local:
-      df_subreddit.to_pickle(subreddit_name+".pkl")
+      filename = subreddit_name + ".csv"
+      if self.compress:
+        compression_opts = dict(method='zip', archive_name=filename)
+        df_subreddit.to_csv(os.path.join(self.outpath, filename), index=False, compression=compression_opts) 
+      else:
+        df_subreddit.to_csv(os.path.join(self.outpath, filename), index=False)
     return df_subreddit
 
   """static loader of pickled subreddit"""
   def load_pickled_sub(subreddit_path):
     return pd.read_pickle(subreddit_path)
 
+# python Downloader.py outpath comment_count subreddits
+def main(argv):
+  dloader = Downloader(outpath = argv[0], max_comment_count = int(argv[1]))
+  for subreddit in argv[2:]:
+    dloader.fetch_subreddit(subreddit)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
